a49546044b49d1c25bd01f89e378e8e77c02a0b1
Giorgi Abashidzee <abashidze.official@gmail.com>
2023-09-01 22:52:45 +0400

    NewApdate

--- src/components/Back.tsx
+++ src/components/Back.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './styles.css';
-
+import bgback from '../images/bg-main-mobile.png';
+import backcard from '../images/bg-card-back.png';
+import bgcardfront from '../images/bg-card-front.png';
 function Back() {
     return (
         <div className="back-container">
             <div className="back-main">
-                <div className="back-card"></div>
+                <img src={bgback} alt="bg" className="back" width="375"></img>
+                <div className="back-card">
+                    <div className="card-bg">
+                        <img src={backcard} alt="bgcard" className="bg-card" width="276" height="147"></img>
+                        <span className="backcardtxt">000</span>
+                    </div>
+                    <div className="card-front">
+                        <span className="cardbacknumber">0000 0000 0000 0000 </span>
+                        <img src={bgcardfront} alt="cardfront" className="card-fr-front" width="276" height="147"></img>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/Front.tsx
+++ src/components/Front.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './styles.css';
+import bgfront from '../images/bg-main-mobile.png';
+import bgcard from '../images/bg-card-back.png';
+import cardfront from '../images/bg-card-front.png';
+
+
 
 function Front() {
     return (
         <div className="front-container">
             <div className="front-main">
-                <div className="front-card"></div>
+                <img src={bgfront} alt="bg" className="bg-front" width="375"></img>
+                <div className="front-card">
+                    <div className="card-bg">
+                        <img src={bgcard} alt="bgcard" className="bg-card" width="276" height="147"></img>
+                        <span className="bgcardtxt">000</span>
+                    </div>
+                    <div className="card-front">
+                        <span className="cardfrontnumber">0000 0000 0000 0000 </span>
+                        <img src={cardfront} alt="cardfront" className="card-fr-front" width="276" height="147"></img>
+                    </div>
+                </div>
             </div>
         </div>
     )
